d88945c041a69d4e6ec6594f06203ee4952ddc6d
chirag <chiragiem36@gmail.com>
2017-06-24 09:23:11 +0530

    added error reporting route

---
+++ routes/era.js
@@ -0,0 +1,19 @@
+var router = require('express').Router()
+var cookie = require('cookie')
+var jwt = require('jsonwebtoken')
+var mongo = require('mongodb').MongoClient
+var authkey = "155975ATpoRPi5h593ea16c"
+
+router.post('/:domain/:subdomain',function(req,res){
+  mongo.connect('mongodb://localhost:27018/oniv',function(err,db){
+    if(err){
+      db.close()
+      res.status(500)
+      res.end()
+    } else {
+      db.collection(req.params.domain).update({_id:req.params.subdomain},{$addToSet:{"recentErrors":req.body.error}})
+      db.close()
+      res.end()
+    }
+  })
+})
