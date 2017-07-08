0107178368d9dbb4147733d203247be8372f75a4
chirag <chiragiem36@gmail.com>
2017-07-09 00:36:34 +0530

    fixed download module to create overview attendance report

--- routes/download.js
+++ routes/download.js
@@ -5,23 +5,28 @@ var cookie = require('cookie')
 var fs = require('fs')
 var xl = require('excel4node')
 var dayName = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-var months = ["January","February","March","April","May","June"]
+var months = ["January","February","March","April","May","June","July","Augut","September","October","November","December"]
 
-router.get('/attendanceOverview/:college/:department/:batch/:faculty_id/:subject',function(req,res){
+router.get('/attendanceOverview/:school/:batch/:section/:faculty_id/:subject',function(req,res){
+  console.log(req.url)
   var cookies = cookie.parse(req.headers.cookie || '')
   jwt.verify(cookies.user,'uit attendance login',function(err,decoded){
     if(!err){
       console.log("Excell sheet requested")
       mongo.connect('mongodb://localhost:27018/data',function(err,db){
+        var classes_held=[];
         if(!err){
-          db.collection('classes').findOne({_id:req.params.college + '/' + req.params.department + '/' + req.params.batch},function(err,itm){
+          db.collection('faculty').findOne({_id:req.params.faculty_id},{"current_classes":1},function(err,item){
             if(!err){
-              var classes_held=[];
-              db.collection('faculty').findOne({_id:req.params.faculty_id},{"current_classes":1},function(err,item){
-                item.current_classes.forEach(function(a,b){
-                  if(a._id === req.params.college + '/' + req.params.department + '/' + req.params.batch ){
-                    classes_held = a.classes_held
-
+              item.current_classes.forEach(function(a,b){
+                if(a._id === req.params.school + '/' + req.params.batch + '/' + req.params.section ){
+                  classes_held = a.classes_held
+                }
+              })
+              }
+            })
+          db.collection('classes').findOne({_id:req.params.school + '/' + req.params.batch + '/' + req.params.section},function(err,itm){
+            if(!err){
                     var data = itm.student_data
                     var wb = new xl.Workbook()
                     var ws = wb.addWorksheet('attendance-sheet')
@@ -30,32 +35,41 @@ router.get('/attendanceOverview/:college/:department/:batch/:faculty_id/:subject
                     ws.column(1).setWidth(25)
                     ws.cell(1,2).string('Name').style(style)
                     ws.column(2).setWidth(25)
-                    ws.cell(1,3).string('Classes Taken').style(style)
+                    ws.cell(1,3).string('Classes Not Taken').style(style)
                     ws.column(3).setWidth(13)
-                    ws.cell(1,4).string('Attendance Percentage').style(style)
+                    ws.cell(1,4).string('Absent Percentage').style(style)
+                    ws.cell(1,5).string('Present Percentage').style(style)
+                    ws.column(4).setWidth(13)
+                    ws.column(5).setWidth(13)
                     for(var i=0;i<data.length;i++){
                       console.log(data[i])
-                      ws.cell(i+2,1).string(data[i].enroll_number)
-                      ws.column(1).setWidth(25)
-                      ws.cell(i+2,2).string(data[i].name)
-                      ws.column(2).setWidth(25)
-                      if(data[i].hasOwnProperty(req.params.subject) && data[i][req.params.subject].hasOwnProperty("attendance") ){
-                        ws.cell(i+2,3).number(data[i][req.params.subject].attendance.length)
-                        ws.column(3).setWidth(13)
-                        ws.cell(i+2,4).number(Math.ceil((data[i][req.params.subject].attendance.length/classes_held.length)*100))
-                        ws.column(4).setWidth(13)
-                      } else {
-                        ws.cell(i+2,3).number(0)
-                        ws.column(3).setWidth(13)
-                        ws.cell(i+2,4).number(0)
-                        ws.column(4).setWidth(25)
+                      if(data[i].name != null && data[i].enroll_number != null){
+                        ws.cell(i+2,1).string(data[i].enroll_number)
+                        ws.column(1).setWidth(25)
+                        ws.cell(i+2,2).string(data[i].name)
+                        ws.column(2).setWidth(25)
+                        if(data[i].hasOwnProperty(req.params.subject) && data[i][req.params.subject].hasOwnProperty("absent") ){
+                          ws.cell(i+2,3).number(data[i][req.params.subject].absent.length)
+                          ws.column(3).setWidth(13)
+                          ws.cell(i+2,4).number(Math.ceil((data[i][req.params.subject].absent.length/classes_held.length)*100))
+                          ws.column(4).setWidth(13)
+                          ws.cell(2+i,5).number(100-Math.ceil((data[i][req.params.subject].absent.length/classes_held.length)*100)).style(style)
+                          ws.column(5).setWidth(13)
+                        } else {
+                          ws.cell(i+2,3).number(0)
+                          ws.column(3).setWidth(13)
+                          ws.cell(i+2,4).number(0)
+                          ws.cell(i+2,5).number(100)
+                          ws.column(4).setWidth(25)
+                          ws.column(5).setWidth(25)
+                        }
                       }
                     }
-                    wb.write(__dirname + '/xlxs/' + req.params.college + '-' + req.params.department + '-' + req.params.batch + '[attendance-report].xlxs',function(err,stats){
+                    wb.write(__dirname + '/xlxs/' + req.params.school + '-' + req.params.section + '-' + req.params.batch + '[attendance-report].xlxs',function(err,stats){
                       if(!err){
                         res.setHeader('Content-Type','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-                        res.setHeader('Content-Disposition','attachment; filename=' + req.params.college + '-' + req.params.department + '-' + req.params.batch + '[attendance-report].xlxs')
-                        fs.createReadStream(__dirname + '/xlxs/' + req.params.college + '-' + req.params.department + '-' + req.params.batch + '[attendance-report].xlxs').pipe(res)
+                        res.setHeader('Content-Disposition','attachment; filename=' + req.params.school + '-' + req.params.section + '-' + req.params.batch + '[attendance-report].xlxs')
+                        fs.createReadStream(__dirname + '/xlxs/' + req.params.school + '-' + req.params.section + '-' + req.params.batch + '[attendance-report].xlxs').pipe(res)
                       } else {
                         console.log(err)
                         db.close()
@@ -63,15 +77,12 @@ router.get('/attendanceOverview/:college/:department/:batch/:faculty_id/:subject
                         res.end()
                       }
                     })
+                  } else {
+                    console.log("document not found")
+                    db.close()
+                    res.status(404)
+                    res.end()
                   }
-                })
-              })
-            } else {
-              console.log("document not found")
-              db.close()
-              res.status(404)
-              res.end()
-            }
           })
         } else {
           console.log("failed to connect to db")
@@ -88,64 +99,97 @@ router.get('/attendanceOverview/:college/:department/:batch/:faculty_id/:subject
   })
 })
 
-router.get('/attendanceDetailed/:college/:department/:batch/:faculty_id/:subject',function(req,res){
+/*
+router.get('/attendanceDetailed/:school/:batch/:section/:faculty_id/:subject',function(req,res){
   var cookies = cookie.parse(req.headers.cookie || '')
   jwt.verify(cookies.user,'uit attendance login',function(err,decoded){
     if(!err){
       console.log("Excell sheet requested")
       mongo.connect('mongodb://localhost:27018/data',function(err,db){
         if(!err){
-          db.collection('classes').findOne({_id:req.params.college + '/' + req.params.department + '/' + req.params.batch},function(err,itm){
-            if(!err){
-              var classes_held=[];
-              db.collection('faculty').findOne({_id:req.params.faculty_id},{"current_classes":1},function(err,item){
-                item.current_classes.forEach(function(a,b){
-                  if(a._id === req.params.college + '/' + req.params.department + '/' + req.params.batch ){
-                    classes_held = a.classes_held
-                    classes_held.sort(function(a, b) {
-                      return a - b;
-                    })
+          var classes_held=[];
+          db.collection('faculty').findOne({_id:req.params.faculty_id},{"current_classes":1},function(err,item){
+            item.current_classes.forEach(function(a,b){
+              if(a._id === req.params.school + '/' + req.params.batch + '/' + req.params.section ){
+                classes_held = a.classes_held
+              }
+                classes_held = classes_held.sort(function(nlmn,dnln){
+                  return dnln - nlmn;
+                })
+              })
+            })
 
+          db.collection('classes').findOne({_id:req.params.school + '/' + req.params.batch + '/' + req.params.section},function(err,itm){
+            if(!err){
                     var data = itm.student_data
                     var wb = new xl.Workbook()
-                    var ws = wb.addWorksheet('detailed-attendance-sheet')
-                    var style = wb.createStyle({font: {color: '#FF0800',size: 12},numberFormat: '$#,##0.00; ($#,##0.00); -'})
-                    var present = wb.createStyle({border: {left:{style:'thin',color:'black'},right:{style:'thin',color:'black'},top:{style:'thin',color:'black'},bottom:{style:'thin',color:'black'}},fill: {type:'pattern',patternType:'lightUp',fgColor: '#00ff00'}})
-                    var absent = wb.createStyle({border: {left:{style:'thin',color:'black'},right:{style:'thin',color:'black'},top:{style:'thin',color:'black'},bottom:{style:'thin',color:'black'}},fill: {type:'pattern',patternType:'lightUp',fgColor: '#FF0800'}})
-                    ws.cell(1,1).string('Enrollment Number').style(style)
-                    ws.column(1).setWidth(25)
-                    ws.cell(1,2).string('Name').style(style)
-                    ws.column(3).setWidth(25)
-                    var list = []
-                    classes_held.forEach(function(x,i){
-                      var d = new Date(x);
-                      if( d.getMonth() == 3){
-                        list.push(x)
+                    var ws = {}
+                    var style = wb.createStyle({font: {color: '#FF0800',size: 12},numberFormat: '$#,##0.00; ($#,##0.00); -'
+                  })
+                    var present = wb.createStyle({border: {left:{style:'thin',color:'black'},right:{style:'thin',color:'black'},top:{style:'thin',color:'black'},bottom:{style:'thin',color:'black'}},fill: {type:'pattern',patternType:'lightUp',fgColor: '#00ff00'
+                  }})
+                    var absent = wb.createStyle({border: {left:{style:'thin',color:'black'},right:{style:'thin',color:'black'},top:{style:'thin',color:'black'},bottom:{style:'thin',color:'black'}},fill: {type:'pattern',patternType:'darkVerical',fgColor: '#ef2b2b'
+                  }})
+                    var current_month = ""
+                    var ntx=0;
+                    classes_held.forEach(function(x,n){
+                      var d = new Date(x)
+                      if(n===0){
+                        current_month = months[d.getMonth()]
+                        ws[months[d.getMonth()]] = wb.addWorksheet(ws[months[d.getMonth()]])
+                        if(ntx===0){
+                          data.forEach(function(zl,en){
+                            if(zl.name && zl.enroll_number){
+                              ws[months[d.getMonth()]].cell(2+en,1).string(zl.enroll_number)
+                              ws[months[d.getMonth()]].cell(2+en,2).string(zl.name)
+                              ws[current_month].column(1).setWidth(15)
+                              ws[current_month].column(2).setWidth(25)
+                            }
+                          })
+                        }
+                      } else if(current_month != months[d.getMonth()]){
+                        ntx = 0
+                        current_month = months[d.getMonth()]
+                        ws[months[d.getMonth()]] = wb.addWorksheet(ws[months[d.getMonth()]])
+                        if(ntx===0){
+                          data.forEach(function(zl,en){
+                            if(zl.name && zl.enroll_number){
+                              ws[months[d.getMonth()]].cell(2+en,1).string(zl.enroll_number)
+                              ws[months[d.getMonth()]].cell(2+en,2).string(zl.name)
+                              ws[current_month].column(1).setWidth(15)
+                              ws[current_month].column(2).setWidth(25)
+                            }
+                          })
+                        }
                       }
+                      ws[months[d.getMonth()]].cell(1,3+ntx).string(d.getDate() + ' ' + months[Number(d.getMonth())] + ', ' + dayName[d.getDay()])
+                      ws[months[d.getMonth()]].column(3+ntx).setWidth(15)
+                      ntx++;
                     })
-                    list.forEach(function(r,n){
-                      var d = new Date(r)
-                      ws.cell(1,3+n).string(  dayName[d.getDay()] + ' ' + d.getDate() + ', ' + months[Number(d.getMonth())])
-                      ws.column(3+n).setWidth(15)
-                    })
-                    console.log(list)
 
-                    data.forEach(function(y,j){
-                      ws.cell(2+j,1).string(y.enroll_number)
-                      ws.cell(2+j,2).string(y.name)
-                      ws.column(2).setWidth(35)
-                      if( y.hasOwnProperty(req.params.subject) ){
-                        y[req.params.subject].attendance.forEach(function(l,m){
-                          if(list.indexOf(l)> -1){
-                            ws.cell(2+j,3+list.indexOf(l)).style(present)
-                            ws.column(3+list.indexOf(l)).setWidth(15)
-                          } else {
+                    current_month = ""
 
-                          }
+                    data.forEach(function(te,ne){
+                      if(y.name != null && y.enroll_number != null && y.hasOwnProperty(req.params.subject) && y[req.params.subject].hasOwnProperty('absent') ){
+                        var absns = y[req.params.subject].absent.sort(function(abl,dbl){
+                          return abl -dbl
                         })
-                      } else {
+                        var nty = 0
+                        absns.forEach(function(l,m){
+                          var crdt = new Date()
+                          if( m===0 ){
+                            nty = 0
+                            current_month = d.getMonth()
+                          } else if( current_month != d.getMonth()){
+                            nty=0;
+                            current_month = d.getMonth()
+                          }
+                          ws[months[months[d.getMonth()]]].cell(2+j,3+nx).style(absent)
+                          ws[months[months[d.getMonth()]].column(3+nx).setWidth(15)
+                        })
                       }
                     })
+
                     wb.write(__dirname + '/xlxs/' + req.params.college + '-' + req.params.department + '-' + req.params.batch + '[attendance-report].xlxs',function(err,stats){
                       if(!err){
                         res.setHeader('Content-Type','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
@@ -159,11 +203,6 @@ router.get('/attendanceDetailed/:college/:department/:batch/:faculty_id/:subject
                       }
                     })
                   } else {
-
-                  }
-                })
-              })
-            } else {
               console.log("document not found")
               db.close()
               res.status(404)
@@ -184,5 +223,7 @@ router.get('/attendanceDetailed/:college/:department/:batch/:faculty_id/:subject
     }
   })
 })
+
+*/
 
 module.exports = router
