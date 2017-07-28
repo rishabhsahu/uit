39c5ec2ccd52457d599563261c5f5a66bb4fdde5
Chirag Jain <chiragiem36@gmail.com>
2017-07-28 11:56:14 +0530

    Route to add Class teacher

--- routes/admin.js
+++ routes/admin.js
@@ -293,6 +293,38 @@ router.post('/addnewfaculty',function(req,res){
 }
 })
 
+router.post('/batchsettings',function(req,res){
+  console.log(req.body)
+  if(!cookie){
+    errRequest("http://localhost:80/error/nodejsErr/admin","cookies",err)
+    res.status(500)
+    res.end()
+  } else {
+  var cookies = cookie.parse(req.headers.cookie || '')
+  jwt.verify(cookies.user,'uit attendance login',function(err,decoded){
+    if(!err){
+      mongo.connect('mongodb://localhost:27018/data',function(err,db){
+        if(err){
+          errRequest("http://localhost:80/error/mongoErr/admin","mongodb",err)
+          db.close()
+          res.status(500)
+          res.end()
+        } else {
+          db.collection("classes").update({_id:req.body.batch},{$set:{"class_teacher":req.body.class_teacher}})
+          db.close()
+          res.status(200)
+          res.end()
+        }
+      })
+    } else {
+      errRequest("http://localhost:80/error/nodejsErr/admin","jwt",err)
+      res.status(401)
+      res.end()
+    }
+  })
+}
+})
+
 router.post('/assignFacultyNewBatch/:faculty_id',function(req,res){
   console.log('assignFacultyNewBatch request');
   var cookies = cookie.parse(req.headers.cookie || '')
