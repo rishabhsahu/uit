7b2d341def827e90578815493c330f9dbea1868b
chirag jain <chiragiem36@gmail.com>
2017-08-11 00:40:54 +0530

    Option to add empty class. Reporting errors to server

--- routes/admin/new.js
+++ routes/admin/new.js
@@ -9,91 +9,43 @@ const root = __dirname
 const request = require('request')
 const qs = require('querystring')
 
+const serverRequest = function(res){
+  request(this,function(err,resp,body){
+		console.log(body,"body")
+		if(!err && resp.statusCode === 200){
+			res.status(200)
+			res.end()
+		} else {
+			console.log(err)
+			res.status(504)
+			res.end()
+		}
+	})
+}
+
+const errRequest = function(u,m,e){
+  serverRequest.call({
+    url:u + "/" + m,
+    method: 'post',
+    body: e,
+    json: true
+  },res)
+}
+
 function addNewBatch(req,res){
+  console.log(req.url);
   var cookies = cookie.parse(req.headers.cookie || '')
   if(!cookie){
-    errRequest("http://localhost:80/error/nodejsErr/admin","cookies",err)
+    errRequest("http://13.126.16.198:80/error/nodejsErr/admin","cookies",err,res)
     res.status(500)
     res.end()
   } else {
   jwt.verify(cookies.user,'uit attendance login',function(err,decoded){
     if(!err){
-      var std = []
-      var form = formidable.IncomingForm()
-      form.encoding = 'utf-8'
-      form.uploadDir = root + "/data"
-      form.parse(req,function(err,fields,files){
-        if(err){
-          errRequest("http://localhost:80/error/nodejsErr/admin","formidable",err)
-        } else {
-          console.log(files)
-        }
-      })
-
-      form.on('file',function(name,file){
-        var name = path.basename(file.path)
-        var data = fs.readFileSync(root + "/data/" + name)
-        data = data.toString()
-        var list = data.split("\n")
-				const class_data = {}
-				class_data.students = []
-				class_data.student_data = []
-				list.forEach((x,i)=>{
-					const obj = {}
-          entry = x.replace("\r","")
-					let add;
-					console.log(entry,0)
-					if(entry.indexOf('\"')>0 && entry.indexOf('\"')<entry.length){
-						add = entry.split('\"')[1]
-						console.log(add)
-						entry = entry.replace('\"' + add + '\"','')
-						console.log(entry,1)
-					}
-          if(entry.length !=0 || entry != ""){
-						const ent = entry.split(',')
-						if(ent.length>5){
-		          obj.enroll_number = ent[0].toLowerCase()
-		          const enrl = ent[0]
-		          obj.name = ent[1].toLowerCase()
-		          obj.pname = ent[2].toLowerCase()
-		          if(add){
-                obj.add = add.toLowerCase()
-              } else {
-                obj.add = ent[3].toLowerCase()
-              }
-		          obj.city = ent[4].toLowerCase()
-		          obj.mobiles = {}
-							for(let ni=5;ni<9;ni++){
-								if(ni===5){
-									if(ent[ni]){
-										obj.mobiles.parent1 = ent[ni]
-									} else {
-										res.status(400)
-										res.end()
-									}
-								} else {
-									if(ent[ni]){
-										switch(ni){
-											case 6:
-												obj.mobiles.parent2 = ent[ni]
-												break;
-
-											case 7:
-												obj.mobiles.sn = ent[ni]
-												break;
-
-											case 8:
-												obj.mobiles.other = ent[ni]
-												break;
-										}
-									}
-								}
-						}
-            class_data.students.push(obj)
-            class_data.student_data.push(obj)
-					}
-					}
-				})
+      if(req.params.m == 0){
+        const class_data = {}
+        class_data.students = []
+        class_data.student_data = []
         class_data._id = req.params.domain_name + '/' + req.params.batch + '/' + req.params.section
         class_data.batch = req.params.batch
         class_data.school = req.params.school
@@ -103,10 +55,9 @@ function addNewBatch(req,res){
         class_data.prev_faculties = {}
         class_data.tests = {}
         class_data.current_faculties = []
-        console.log(class_data)
         mongo.connect('mongodb://localhost:27018/data',function(err,db){
           if(err){
-            errRequest("http://localhost:80/error/mongoErr/admin","mongodb",err)
+            errRequest("http://13.126.16.198:80/error/mongoErr/admin","mongodb",err,res)
             db.close()
             res.status(500)
             res.end()
@@ -118,9 +69,110 @@ function addNewBatch(req,res){
             res.end()
           }
         })
-      })
+      } else {
+        const std = []
+        const form = formidable.IncomingForm()
+        form.encoding = 'utf-8'
+        form.uploadDir = root + "/data"
+        form.parse(req,function(err,fields,files){
+          if(err){
+            errRequest("http://13.126.16.198:80/error/nodejsErr/admin","formidable",err,res)
+          } else {
+            console.log(files)
+          }
+        })
+        form.on('file',function(name,file){
+          const nme = path.basename(file.path)
+          const data = fs.readFileSync(root + "/data/" + nme)
+          data = data.toString()
+          const list = data.split("\n")
+  				const class_data = {}
+  				class_data.students = []
+  				class_data.student_data = []
+  				list.forEach((x,i)=>{
+  					const obj = {}
+            entry = x.replace("\r","")
+  					let add;
+  					console.log(entry,0)
+  					if(entry.indexOf('\"')>0 && entry.indexOf('\"')<entry.length){
+  						add = entry.split('\"')[1]
+  						console.log(add)
+  						entry = entry.replace('\"' + add + '\"','')
+  						console.log(entry,1)
+  					}
+            if(entry.length !=0 || entry != ""){
+  						const ent = entry.split(',')
+  						if(ent.length>5){
+  		          obj.enroll_number = ent[0].toLowerCase()
+  		          const enrl = ent[0]
+  		          obj.name = ent[1].toLowerCase()
+  		          obj.pname = ent[2].toLowerCase()
+  		          if(add){
+                  obj.add = add.toLowerCase()
+                } else {
+                  obj.add = ent[3].toLowerCase()
+                }
+  		          obj.city = ent[4].toLowerCase()
+  		          obj.mobiles = {}
+  							for(let ni=5;ni<9;ni++){
+  								if(ni===5){
+  									if(ent[ni]){
+  										obj.mobiles.parent1 = ent[ni]
+  									} else {
+  										res.status(400)
+  										res.end()
+  									}
+  								} else {
+  									if(ent[ni]){
+  										switch(ni){
+  											case 6:
+  												obj.mobiles.parent2 = ent[ni]
+  												break;
+
+  											case 7:
+  												obj.mobiles.sn = ent[ni]
+  												break;
+
+  											case 8:
+  												obj.mobiles.other = ent[ni]
+  												break;
+  										}
+  									}
+  								}
+  						}
+              class_data.students.push(obj)
+              class_data.student_data.push(obj)
+  					}
+  					}
+  				})
+          class_data._id = req.params.domain_name + '/' + req.params.batch + '/' + req.params.section
+          class_data.batch = req.params.batch
+          class_data.school = req.params.school
+          class_data.domain_name = req.params.domain_name
+          class_data.class = req.params.cls
+          class_data.section = req.params.section
+          class_data.prev_faculties = {}
+          class_data.tests = {}
+          class_data.current_faculties = []
+          console.log(class_data)
+          mongo.connect('mongodb://localhost:27018/data',function(err,db){
+            if(err){
+              errRequest("http://13.126.16.198:80/error/mongoErr/admin","mongodb",err,res)
+              db.close()
+              res.status(500)
+              res.end()
+            } else {
+              db.collection('classes').insert(class_data)
+              db.collection('admin').update({_id:decoded.name},{$addToSet:{"batches":{"batch":class_data.batch,"_id":class_data._id,"class":req.params.cls}}})
+              db.close()
+              res.status(200)
+              res.end()
+            }
+          })
+        })
+      }
     } else {
-      errRequest("http://localhost:80/error/nodejsErr/admin","jwt",err)
+      errRequest("http://13.126.16.198:80/error/nodejsErr/admin","jwt",err,res)
       res.status(401)
       res.end()
     }
@@ -131,7 +183,7 @@ function addNewBatch(req,res){
 function addNewFaculty(req,res){
   console.log(req.body)
   if(!cookie){
-    errRequest("http://localhost:80/error/nodejsErr/admin","cookies",err)
+    errRequest("http://13.126.16.198:80/error/nodejsErr/admin","cookies",err,res)
     res.status(500)
     res.end()
   } else {
@@ -140,7 +192,7 @@ function addNewFaculty(req,res){
     if(!err){
       mongo.connect('mongodb://localhost:27018/data',function(err,db){
         if(err){
-          errRequest("http://localhost:80/error/mongoErr/admin","mongodb",err)
+          errRequest("http://13.126.16.198:80/error/mongoErr/admin","mongodb",err,res)
           db.close()
           res.status(500)
           res.end()
@@ -150,7 +202,7 @@ function addNewFaculty(req,res){
           db.collection('faculty').insert(req.body)
           db.collection('admin').update({_id:decoded.name},{$addToSet:{"faculties":{"id": req.body._id,"name": req.body.name,"recent_messages":{},"total_messages":{}}}})
           request({
-            url:'http://ec2-13-126-212-231.ap-south-1.compute.amazonaws.com/sendsms/facultyotp',
+            url:'http://13.126.16.198:80/sendsms/facultyotp',
             body: {name:req.body.name,username:req.body._id,mobile:req.body.mobile,otp:req.body.otp,school:req.body.school},
             json:true,
             method:'POST'
@@ -171,7 +223,7 @@ function addNewFaculty(req,res){
         }
       })
     } else {
-      errRequest("http://localhost:80/error/nodejsErr/admin","jwt",err)
+      errRequest("http://13.126.16.198:80/error/nodejsErr/admin","jwt",err,res)
       res.status(401)
       res.end()
     }
@@ -182,7 +234,7 @@ function addNewFaculty(req,res){
 function addNewStudent(req,res){
   let cookies = cookie.parse(req.headers.cookie || '')
   if(!cookie){
-    errRequest("http://localhost:80/error/nodejsErr/admin","cookies",err)
+    errRequest("http://13.126.16.198:80/error/nodejsErr/admin","cookies",err,res)
     res.status(500)
     res.end()
   } else {
@@ -190,7 +242,7 @@ function addNewStudent(req,res){
       if(!err){
         mongo.connect('mongodb://localhost:27018/data',function(err,db){
           if(err){
-            errRequest("http://localhost:80/error/mongoErr/admin","mongodb",err)
+            errRequest("http://13.126.16.198:80/error/mongoErr/admin","mongodb",err,res)
             db.close()
             res.status(500)
             res.end()
@@ -240,7 +292,7 @@ function addNewStudent(req,res){
           }
         })
       } else {
-        errRequest("http://localhost:80/error/nodejsErr/admin","jwt",err)
+        errRequest("http://13.126.16.198:80/error/nodejsErr/admin","jwt",err,res)
         res.status(401)
         res.end()
       }
